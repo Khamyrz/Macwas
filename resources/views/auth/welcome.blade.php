cccb0550a59911d8693c8e872fef6a077c52797c
Khamyrz <Khamyrbautista@gmail.com>
2025-11-08 23:21:54 +0800

    SDDDDF

--- resources/views/auth/welcome.blade.php
+++ resources/views/auth/welcome.blade.php
@@ -1,3 +1,11 @@
+@php
+// Set security headers for this page (required by securityheaders.com)
+header('Strict-Transport-Security: max-age=31536000; includeSubDomains');
+header('X-Frame-Options: SAMEORIGIN');
+header('X-Content-Type-Options: nosniff');
+header('Referrer-Policy: strict-origin-when-cross-origin');
+header('Permissions-Policy: geolocation=(), microphone=(), camera=(), payment=(), usb=(), magnetometer=(), gyroscope=(), speaker=()');
+@endphp
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
